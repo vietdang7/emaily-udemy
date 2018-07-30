b5edd4bce574e48a6f6ec2f4b62c41dd258b966e
Viet Dang <thedangs7@gmail.com>
2018-07-30 10:43:14 +0300

    fix bug

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // check which env to export the keys
-if (ProcessingInstruction.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
     module.exports = require('./dev');
